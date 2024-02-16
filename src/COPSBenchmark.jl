1a6008bb82ee21a844deeb411e129d92450f9ff6
fpacaud <francoispacaud8@gmail.com>
2024-02-16 09:21:21 +0100

    add new instances

--- src/COPSBenchmark.jl
+++ src/COPSBenchmark.jl
@@ -5,6 +5,7 @@ using JuMP
 
 include("bearing.jl")
 include("camshape.jl")
+include("catmix.jl")
 include("elec.jl")
 include("gasoil.jl")
 include("marine.jl")
@@ -12,5 +13,6 @@ include("pinene.jl")
 include("robot.jl")
 include("rocket.jl")
 include("steering.jl")
+include("torsion.jl")
 
 end # module COPSBenchmark
