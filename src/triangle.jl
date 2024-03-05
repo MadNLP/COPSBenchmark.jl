15d543019e6a7ddb07fe5769639edb00e6301173
tmigot <tangi.migot@gmail.com>
2024-03-05 03:24:57 -0500

    Add triangle problem (#6)

---
+++ src/triangle.jl
@@ -0,0 +1,64 @@
+# Minimize the time taken for a robot arm to travel between two points.
+
+#  This is problem 18 in the COPS (Version 3) collection of 
+#   E. Dolan and J. More
+#   see "Benchmarking Optimization Software with COPS"
+#   Argonne National Labs Technical Report ANL/MCS-246 (2004)
+
+# This file has been adapted from https://github.com/JuliaSmoothOptimizers/OptimizationProblems.jl
+
+include(joinpath("..", "data", "triangle.jl"))
+
+function triangle_model(x0 = xe, TRIS::Vector{Int64} = Tr, Const::Vector{Int64} = Constants)
+  τ = 0.0
+  n = length(x0)
+  N = Int(div(n, 2))
+  E = Int(div(length(TRIS), 3))
+
+  lvar = -Inf * ones(n)
+  lvar[Const] = x0[Const]
+  lvar[Const .+ N] = x0[Const .+ N]
+
+  uvar = Inf * ones(n)
+  uvar[Const] = x0[Const]
+  uvar[Const .+ N] = x0[Const .+ N]
+
+  nlp = Model()
+
+  @variable(nlp, lvar[i] <= x[i = 1:n] <= uvar[i], start = x0[i])
+
+  @objective(
+    nlp,
+    Min,
+    sum(
+      sum(
+        (1 * x[TRIS[e + E] + N * i] - x[TRIS[e] + N * i])^2 +
+        (2 * x[TRIS[e + 2 * E] + N * i] - x[TRIS[e + E] + N * i] - x[TRIS[e] + N * i])^2 / 3 for
+        i = 0:1
+      ) / (
+        2 * (
+          2 * (
+            (x[TRIS[e + E]] - x[TRIS[e]]) * (x[TRIS[e + 2 * E] + N] - x[TRIS[e] + N]) -
+            (x[TRIS[e + 2 * E]] - x[TRIS[e]]) * (x[TRIS[e + E] + N] - x[TRIS[e] + N])
+          ) / sqrt(3)
+        )
+      ) for e = 1:E
+    )
+  )
+
+  for e = 1:E
+    @constraint(
+      nlp,
+      2 * (
+        (x[TRIS[e + E]] - x[TRIS[e]]) * (x[TRIS[e + 2 * E] + N] - x[TRIS[e] + N]) -
+        (x[TRIS[e + 2 * E]] - x[TRIS[e]]) * (x[TRIS[e + E] + N] - x[TRIS[e] + N])
+      ) / sqrt(3) >= τ
+    )
+  end
+
+  return nlp
+end
+
+triangle_deer_model() = triangle_model(xe_deer, TRIS_deer, Const_deer)
+triangle_pacman_model() = triangle_model(xe_pacman, TRIS_pacman, Const_pacman)
+triangle_turtle_model() = triangle_model(xe_turtle, TRIS_turtle, Const_turtle)
