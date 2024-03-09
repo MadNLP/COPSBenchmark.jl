6acc633c7d74517906cd2953028e46d1be8d0c9f
tmigot <tangi.migot@gmail.com>
2024-03-09 23:36:21 +0100

    Add minsurf

--- src/COPSBenchmark.jl
+++ src/COPSBenchmark.jl
@@ -3,7 +3,6 @@ module COPSBenchmark
 using Random
 using JuMP
 
-
 include("bearing.jl")
 include("chain.jl")
 include("camshape.jl")
@@ -14,6 +13,7 @@ include("gasoil.jl")
 include("glider.jl")
 include("marine.jl")
 include("methanol.jl")
+include("minsurf.jl")
 include("pinene.jl")
 include("polygon.jl")
 include("robot.jl")
